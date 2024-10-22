eb012f6c2b3763c0583ce834289495f48a03ab35
Luckas Peres <97205232+Luckmaiqui@users.noreply.github.com>
2024-10-22 11:08:46 -0300

    Update update_age.py

--- update_age.py
+++ update_age.py
@@ -2,32 +2,25 @@ import datetime
 import re
 import subprocess
 
-# Data de nascimento completa (ano, mês, dia)
-birth_date = datetime.date(1995, 10, 22)  # Exemplo: 22 de outubro de 1995
+birth_date = datetime.date(2003, 12, 29)
 
-# Data atual
+
 today = datetime.date.today()
 
-# Cálculo da idade: verificar se já fez aniversário este ano
 age = today.year - birth_date.year
 if (today.month, today.day) < (birth_date.month, birth_date.day):
-    age -= 1  # Ainda não fez aniversário este ano
+    age -= 1 
 
-# Caminho para o arquivo README
 readme_path = 'README.md'
 
-# Abrir o arquivo README
 with open(readme_path, 'r') as file:
     readme_content = file.read()
 
-# Substituir a idade antiga pela nova (buscando por uma linha "Idade: <número>")
-new_readme_content = re.sub(r'Idade: \d+', f'Idade: {age}', readme_content)
+new_readme_content = re.sub(r'<h3>☼ \d+ Anos</h3>', f'<h3>☼ {age} Anos</h3>', readme_content)
 
-# Escrever as alterações no README
 with open(readme_path, 'w') as file:
     file.write(new_readme_content)
 
-# Comandos Git para fazer commit e enviar as alterações
 subprocess.run(['git', 'add', readme_path])
-subprocess.run(['git', 'commit', '-m', f'Atualizando idade para {age}'])
+subprocess.run(['git', 'commit', '-m', f'Atualizando idade para {age} anos'])
 subprocess.run(['git', 'push'])
