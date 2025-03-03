42f4db9914d4d4e997684dd365f2cb51f6092b31
Luckas Peres <97205232+Luckmaiqui@users.noreply.github.com>
2025-03-03 15:01:52 -0300

    Update update_age.py

--- update_age.py
+++ update_age.py
@@ -3,10 +3,7 @@ import re
 import subprocess
 
 birth_date = datetime.date(2003, 12, 29)
-
-
 today = datetime.date.today()
-
 age = today.year - birth_date.year
 if (today.month, today.day) < (birth_date.month, birth_date.day):
     age -= 1 
@@ -18,9 +15,12 @@ with open(readme_path, 'r') as file:
 
 new_readme_content = re.sub(r'<h3>☼ \d+ Anos</h3>', f'<h3>☼ {age} Anos</h3>', readme_content)
 
-with open(readme_path, 'w') as file:
-    file.write(new_readme_content)
+if new_readme_content != readme_content:  # Só faz commit se houver mudança
+    with open(readme_path, 'w') as file:
+        file.write(new_readme_content)
 
-subprocess.run(['git', 'add', readme_path])
-subprocess.run(['git', 'commit', '-m', f'Atualizando idade para {age} anos'])
-subprocess.run(['git', 'push'])
+    subprocess.run(['git', 'add', readme_path])
+    subprocess.run(['git', 'commit', '-m', f'Atualizando idade para {age} anos'])
+    subprocess.run(['git', 'push'])
+else:
+    print("Idade já está correta, nada para atualizar.")
